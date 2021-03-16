d52a6e664104d99b958d3293e35c81b546e1e0d5
Sean Ho <seanfho@gmail.com>
2021-03-16 17:21:22 -0400

    Solution to problem 6 added to index.js

--- index.js
+++ index.js
@@ -241,10 +241,11 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+function annoyingSong(num){
+        return `${num} bottles of soda on the wall, ${num} bottles of soda, take one down pass it around ${num-1} bottles of soda on the wall`
+      }
 
+  console.log("Task 6:", annoyingSong(5));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 

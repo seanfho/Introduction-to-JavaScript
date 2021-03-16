74835a93ea11fa6b69de6d497870aadc8ccc3929
Sean Ho <seanfho@gmail.com>
2021-03-16 16:37:14 -0400

    Solutions for problem 2 and 3 add to index.js

--- index.js
+++ index.js
@@ -90,10 +90,11 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-    /*add your code here*/
+function dogYears(age){
+    return age * 7;
 }
 
+console.log("Task 2:", dogYears(28));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -123,11 +124,29 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight, age){
+  if(age >= 1){
+    if(weight <= 5){
+      return weight * 0.05;
+    }else if(weight > 5 && weight <= 10){
+      return weight * 0.04;
+    }else if(weight > 10 && weight <= 15){
+      return weight * 0.03;
+    }else if(weight > 15){
+      return weight * 0.02;
+    }
+  }else if(age < 1){
+    if(age >= 2/12 && age < 4/12){
+      return weight * 0.1;
+    }else if(age >= 4/12 && age < 7/12){
+      return weight * 0.05;
+    }else if(age >= 7/12 && age < 1){
+      return weight * 0.04;
+    }
   }
+}
 
-
+console.log("Task 3:", hungryDog(15, 1));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
